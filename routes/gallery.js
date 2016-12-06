6b2e2a4802fc1557e65ad8109c3dc2b22f1826de
Amanda Field <afield788@gmail.com>
2016-12-05 20:47:43 -0500

    styling for landing page and gallery complete

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET gallery view */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('gallery', { title: 'Gallery' });
 });
+
 
 module.exports = router;
